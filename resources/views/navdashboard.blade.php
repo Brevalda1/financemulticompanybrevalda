ff5d72a36eb2899f2fa2ea20728d1272364da205
brevalda <brevalda@gmail.com>
2023-08-28 15:44:52 +0700

    first commit

---
+++ resources/views/navdashboard.blade.php
@@ -0,0 +1,25 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+    <div class="sidebar-sticky">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="/admin/members">
+            
+            Dashboard 
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/items">
+            
+            list item
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/listseller">
+           
+            list seller
+          </a>
+        </li>
+      
+      </ul>
+    </div>
+  </nav>
