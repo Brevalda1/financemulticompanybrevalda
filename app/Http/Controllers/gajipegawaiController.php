7993f2e9f6eb6f8adbe0522e64439ee526e1b67e
brevalda <brevalda@gmail.com>
2023-10-19 22:27:50 +0700

    insert gaji pegawai fix (kurang perhitungan gaji)

--- app/Http/Controllers/gajipegawaiController.php
+++ app/Http/Controllers/gajipegawaiController.php
@@ -21,21 +21,30 @@ class gajipegawaiController extends Controller
             $kop='sss';
             $vop=2131;
             $form_id_pegawai_gaji = $req->form_id_pegawai_gaji;
-         
+            $form_nomor_ktp_pegawai_gaji =$req->form_nomor_ktp_pegawai_gaji;
+            $form_nama_pegawai_gaji =$req->form_nama_pegawai_gaji;
+            $form_jumlah_kehadiran_pegawai_gaji =$req->form_jumlah_kehadiran_pegawai_gaji;
+            $form_rate_pegawai_gaji =$req->form_rate_pegawai_gaji;
+            $form_tambahan_lainlain_pegawai_gaji =$req->form_tambahan_lainlain_pegawai_gaji;
+            $form_keterangan_pegawai_gaji =$req->form_keterangan_pegawai_gaji;
+            $form_total_pegawai_gaji =$req->form_total_pegawai_gaji;
+            $form_nomor_rekening_pegawai_gaji =$req->form_nomor_rekening_pegawai_gaji;
+            $form_nama_bank_pegawai_gaji=$req->form_nama_bank_pegawai_gaji;
+            $form_jabatan_pegawai_gaji=$req->form_jabatan_pegawai_gaji;
             
             $new = new GajiPegawai();
             // $new->add($form_id_pegawai_gaji,$kop,$kop,$vop,$vop,$vop,$kop,$vop,$kop,$kop,$kop);
             $new->id_pegawai_gaji = $form_id_pegawai_gaji;
-            $new->nomor_ktp_pegawai_gaji = $kop;
-            $new->nama_pegawai_gaji = $kop;
-            $new->jumlah_kehadiran_pegawai_gaji=$vop;
-            $new->rate_pegawai_gaji=$vop;
-            $new->tambahan_lainlain_pegawai_gaji=$vop;
-            $new->keterangan_pegawai_gaji=$kop;
-            $new->total_pegawai_gaji=$vop;
-            $new->jabatan_pegawai_gaji = $kop;
-            $new->nomor_rekening_pegawai_gaji =$kop;
-            $new->nama_bank_pegawai_gaji=$kop;
+            $new->nomor_ktp_pegawai_gaji = $form_nomor_ktp_pegawai_gaji;
+            $new->nama_pegawai_gaji = $form_nama_pegawai_gaji;
+            $new->jumlah_kehadiran_pegawai_gaji=$form_jumlah_kehadiran_pegawai_gaji;
+            $new->rate_pegawai_gaji=$form_rate_pegawai_gaji;
+            $new->tambahan_lainlain_pegawai_gaji=$form_tambahan_lainlain_pegawai_gaji;
+            $new->keterangan_pegawai_gaji=$form_keterangan_pegawai_gaji;
+            $new->total_pegawai_gaji=$form_total_pegawai_gaji;
+            $new->jabatan_pegawai_gaji = $form_jabatan_pegawai_gaji;
+            $new->nomor_rekening_pegawai_gaji =$form_nomor_rekening_pegawai_gaji;
+            $new->nama_bank_pegawai_gaji=$form_nama_bank_pegawai_gaji;
       
 
             $new->save();
--- resources/views/formgajipegawai.blade.php
+++ resources/views/formgajipegawai.blade.php
@@ -20,43 +20,43 @@
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Nomor Ktp</label>
-                <input id="form_nomor_ktp_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_nomor_ktp_pegawai_gaji" name="form_nomor_ktp_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Nama Pegawai</label>
-                <input id="form_nama_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_nama_pegawai_gaji" name="form_nama_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Jabatan</label>
-                <input id="form_jabatan_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_jabatan_pegawai_gaji" name="form_jabatan_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Jumlah jam kerja</label>
-                <input id="form_jumlah_kehadiran_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_jumlah_kehadiran_pegawai_gaji" name="form_jumlah_kehadiran_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Rate Pegawai</label>
-                <input id="form_rate_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_rate_pegawai_gaji" name="form_rate_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Tambahan Lain-Lain</label>
-                <input id="form_tambahan_lainlain_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_tambahan_lainlain_pegawai_gaji" name="form_tambahan_lainlain_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Keterangan</label>
-                <input id="form_keterangan_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_keterangan_pegawai_gaji" name="form_keterangan_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Total</label>
-                <input id="form_total_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_total_pegawai_gaji" name="form_total_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Nomor Rekening</label>
-                <input id="form_nomor_rekening_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_nomor_rekening_pegawai_gaji" name="form_nomor_rekening_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="form-group">
                 <label for="cc-payment" class="control-label mb-1">Nama Bank</label>
-                <input id="form_nama_bank_pegawai_gaji" name="cc-payment" type="text" class="form-control" aria-required="true" aria-invalid="false" >
+                <input id="form_nama_bank_pegawai_gaji" name="form_nama_bank_pegawai_gaji" type="text" class="form-control" aria-required="true" aria-invalid="false" >
             </div>
             <div class="row">
                 
